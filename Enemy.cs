b5b98d22ff717483cd80da52d49b34725a670231
unknown <Seth@Shadow.(none)>
2012-01-15 23:57:13 -0700

    added State for Player and Enemy to be used when saving the game

--- Enemy.cs
+++ Enemy.cs
@@ -7,12 +7,25 @@ namespace U5Designs
 {
     class Enemy
     {
+        /** This struct will contain the Players Status **/
+        struct EnemyState
+        {
+            public int e_health;
+            public int e_damage;
+            public double e_speed;
+            public string e_equip_ability;
+            public int e_current_zone;
+
+            // texture...
+
+        };
 
         int health, damage;
         double speed;
         // texture = .... ;
 
-        
+        EnemyState e_state;
+
         public Enemy(int hp, int dam, double spd)
         {
             health = hp;
@@ -21,6 +34,15 @@ namespace U5Designs
             // type = ... ;
         }
 
-        
+
+        /** Like the Player Status update call this every time you need to update an Enemies State before saving **/
+        public void updateState()
+        {
+            e_state.e_health = health;
+            e_state.e_damage = damage;
+            e_state.e_speed = speed;
+
+            // Add in the other State elements that will need to be maintained here..
+        }
     }
 }
--- Player.cs
+++ Player.cs
@@ -7,11 +7,25 @@ namespace U5Designs
 {
     class Player
     {
-        
+        /** This struct will contain the Players State **/
+        struct PlayerState
+        {
+            public int p_health;
+            public int p_damage;
+            public double p_speed;
+            public string p_equip_ability;
+            public int p_current_zone;
+
+            // texture...
+
+        };
+
+        PlayerState p_state;
+
         //player globals here
         int health, damage;
         double speed;
-        //texture .... ;
+        //texture .... ;               
 
         public Player()
         {
@@ -20,6 +34,18 @@ namespace U5Designs
             speed = 2.0;
         }
 
+        /**
+         * Sets the PlayerState elements to the current Player values.  Call this method every update or simply when the state changes.  This will be used to store
+         * the Players State when saving the game.
+         * */
+        public void updateState()
+        {
+            p_state.p_health = health;
+            p_state.p_damage = damage;
+            p_state.p_speed = speed;
 
+            // Put the rest in here like texture, current zone, etc
+
+        }
     }
 }
