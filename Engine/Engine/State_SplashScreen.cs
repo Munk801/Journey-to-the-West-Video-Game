49ab17d9ceb70e5485ff2b825757a10cda3fd892
Stephen Lu <munk801@gmail.com>
2012-01-15 19:54:54 -0700

    Added a Game Objects class and State System skeleton class to the Engine library.  This will handle game objects and states of the game.

---
+++ Engine/Engine/State_SplashScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    class State_SplashScreen : GameObject
+    {
+        public void Update(double elapsedTime)
+        {
+            System.Console.WriteLine("Updating Splash Screen");
+        }
+
+        public void Render()
+        {
+            System.Console.WriteLine("Rendering Splash Screen");
+        }
+    }
+}
