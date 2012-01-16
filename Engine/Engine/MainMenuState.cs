c6152a1fe8e607df72a8043921fd2ebab363dd10
SethSLC <walsh1812@gmail.com>
2012-01-16 00:30:50 -0700

    Added State skeleton classes and base State class to Engine.

---
+++ Engine/Engine/MainMenuState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    /** Main Menu State of the game that will be active while the Main Menu is up **/
+    class MainMenuState : GameState
+    {
+        void Init()
+        {
+        }
+
+        void Cleanup()
+        {
+        }
+
+        void Pause()
+        {
+        }
+
+        void Resume()
+        {
+        }
+
+        void HandleEvents()
+        {
+        }
+
+        void Update()
+        {
+        }
+
+        void Draw()
+        {
+        }
+    }
+}
