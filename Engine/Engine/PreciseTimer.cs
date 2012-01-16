7610b8dce2193bd20561a1d277603f0b3f1ddd66
Stephen Lu <munk801@gmail.com>
2012-01-15 18:30:01 -0700

    Added an Engine Library that we can add classes such as the rendering, update, etc to.  A game loop and timer is already added.  Please add any Game Engine code to this library.

---
+++ Engine/Engine/PreciseTimer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace GameLoop
+{
+    class PreciseTimer
+    {
+        // Since we are calling C, Don't manage security checks
+        [System.Security.SuppressUnmanagedCodeSecurity]
+        [DllImport ("kernel32")]
+        // Retrieves the frequency of the high resolution performance counter
+        private static extern bool QueryPerformanceFrequency(ref long PerformanceFrequency);
+        [System.Security.SuppressUnmanagedCodeSecurity]
+        [DllImport("kernel32")]
+        // Retrieves the current value of the high-resolution performance counter.
+        private static extern bool QueryPerformanceCounter(ref long PerformanceCount);
+        long _ticksPerSecond = 0;
+        long _previousElapsedTime = 0;
+
+        public PreciseTimer()
+        {
+            QueryPerformanceFrequency(ref _ticksPerSecond);
+            GetElapsedTime();
+        }
+
+        public double GetElapsedTime()
+        {
+            long time = 0;
+            QueryPerformanceCounter(ref time);
+            double elapsedTime = (double)(time - _previousElapsedTime);
+            _previousElapsedTime = time;
+            return elapsedTime;
+        }
+    }
+}
