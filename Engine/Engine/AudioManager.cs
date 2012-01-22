d11b0c901a4e4c61e3b71468e206b21bdf88d71a
Stephen Lu <munk801@gmail.com>
2012-01-21 17:10:39 -0700

    Skeleton for Audio Manager and Audio class.  Looking into the best way to structure all the sound data.  SL

---
+++ Engine/Engine/AudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenAL = OpenTK.Audio.OpenAL;
+
+namespace Engine
+{
+    /* Managemenet class for sounds that are to be produced in game.  Stores all the sounds and will played based on triggers in game*/
+    public class AudioManager
+    {
+        /* Hardware can only allow 256 sound channels to be processed at a single time.  Track the max channels and the channels that are being passed through*/
+        readonly int maxSoundChannels = 256;
+        List<int> soundChannels = new List<int>();
+        
+        public AudioManager()
+        {
+            // If there is no default device, return
+
+        }
+
+        public void LoadSound(string soundID, string path)
+        {
+
+        }
+
+        public Sound PlaySound(string soundID)
+        {
+            return null;
+        }
+
+        public void StopSound(Sound sound)
+        {
+        }
+
+        public bool isSoundPlaying(Sound sound)
+        {
+            return false;
+        }
+    }
+}
---
+++ Engine/Engine/Sound.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    /* Represents a sound that is made in the game.*/
+    class Sound
+    {
+    }
+}
