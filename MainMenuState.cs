a9d31dc40978de56dfe71f14ec603463a71aec33
SethSLC <walsh1812@gmail.com>
2012-01-22 23:50:12 -0700

    Finished the Load Game stuff.  Can now display a list of available saved games which can then be chosen.  The chosen game can then be turned into a PlayerState which can be used to finish populating the GameState elements. Also changed it to a .sav file format.

--- MainMenuState.cs
+++ MainMenuState.cs
@@ -21,17 +21,24 @@ namespace U5Designs
     {
 
         // A container which will hold the list of available saved games
-        Stack<PlayerState> savedGames;
+        Stack<XmlNodeList> savedGameStates;
+        Stack<string> savedGameChoices;
 
         // Initialize graphics here for the game playing
         public override void Init(GameEngine eng)
         {
             // Initialize the saved games stack
-            savedGames = new Stack<PlayerState>();
+            savedGameStates = new Stack<XmlNodeList>();
+            savedGameChoices = new Stack<string>();
 
-            // Load saved game data
-            LoadSavedGame();
+            // Setup saved game data 
+            SavedGameDataSetup();
 
+            // Display available saved game states
+            DisplayAvailableSaves(); 
+
+            // TEST //
+            LoadSavedState(1);
         }
 
         // Cleanup any resources you created here
@@ -50,7 +57,6 @@ namespace U5Designs
         public override void HandleEvents(GameEngine eng)
         {
             DealWithKeys(eng);
-
         }
 
         public override void Update(GameEngine eng, FrameEventArgs e)
@@ -90,41 +96,95 @@ namespace U5Designs
         }
 
         /**
-        * This will load the saved game data into memory.  The player can then choose from a list of available saved games if they choose during the menu state
+         * This method will handle setting up the needed structures for Saved Game states.  It will populate the choices data structure, and the state data 
+         * structure.
         * */
-        public void LoadSavedGame()
+        public void SavedGameDataSetup()
         {
-
-            // Parse XML saved game data file and store the information
-
-            using (XmlReader reader = XmlReader.Create("save.xml"))
+            // Parse XML saved game data file and store the information             
+            XmlDocument doc = new XmlDocument();
+            doc.Load("test.sav");
+            XmlNodeList games = doc.GetElementsByTagName("save");            
+            foreach(XmlNode n in games)
             {
-                string str;
-                int val;
-                double dval;
+                // Create a new list of nodes that will contain each data element from a particular saved game state
+                XmlNodeList nd = n.ChildNodes;
 
-                reader.Read();
+                // Push each "saved" game state onto the stack
+                savedGameStates.Push(nd);
 
-                // Read to start <element> that you are looking to parse                
-                reader.ReadToFollowing("pname");
-
-                // Debug
-                Console.WriteLine(reader.ReadString());
-                str = reader.ReadString();
-
-                if (!String.IsNullOrEmpty(str))
+                string str = "";
+                foreach (XmlNode n2 in nd)
                 {
-                    PlayerState ps = new PlayerState(reader.ReadString());
+                    if (n2.Name.CompareTo("p_name") == 0)
+                    {
+                        str += "Name: " + n2.InnerText;
+                    }
+                    if (n2.Name.CompareTo("p_current_zone") == 0)
+                    {
+                        str += " Zone: " + n2.InnerText;
+                    }                    
+                }
+                //Console.WriteLine(str);
+                savedGameChoices.Push(str);
+            }                     
+        }
 
+        /**
+         * This method will display the available saved games to the user and allow for them to choose.  The choice will be used to load the correct
+         * saved game state into play
+         * */
+        public void DisplayAvailableSaves()
+        {
+            Console.WriteLine("Available Saved Games:");
+            foreach (string s in savedGameChoices)
+            {
+                // Need to use this data to create button graphics during the Load Game stage                
+                Console.WriteLine(s);
+            }
+        }
 
-                    // finish with the current <element> and move to the next
-                    reader.ReadEndElement();
+        /**
+         * This method will take the players choice chosen during the Main Menu Load Game and load the appropriate PlayerState
+         * */
+        public PlayerState LoadSavedState(int choice)
+        {
+            PlayerState ps = new PlayerState();
+            XmlNodeList[] n_list = savedGameStates.ToArray();
+            XmlNodeList chosen_state = n_list[choice];
+            List<int> p_abilities = new List<int>();
 
-                    reader.ReadToFollowing("level");
-                    Console.WriteLine("Level: " + reader.ReadString());
+            // Loop over each element in the XML saved state and start populating the PlayerState with it
+            foreach (XmlNode n in chosen_state)
+            {
+                /** Every item that is added to the PlayerState.cs file NEEDS to be accounted for here **/
+                if (n.Name.CompareTo("p_name") == 0)
+                    ps.setName(n.InnerText);
+                if (n.Name.CompareTo("p_current_zone") == 0)
+                    ps.setZone(Convert.ToInt32(n.InnerText));
+                if (n.Name.CompareTo("p_health") == 0)
+                    ps.setHealth(Convert.ToInt32(n.InnerText));
+                if (n.Name.CompareTo("p_speed") == 0)
+                    ps.setSpeed(Convert.ToDouble(n.InnerText));
+                if (n.Name.CompareTo("p_damage") == 0)
+                    ps.setDamage(Convert.ToInt32(n.InnerText));
+
+                if (n.Name.CompareTo("abilities") == 0)
+                {
+                    string[] a_str = n.InnerText.Split(' ');
+                    foreach (string s in a_str)
+                    {
+                        // Convert each ability to an integer and add it to the list
+                        p_abilities.Add(Convert.ToInt32(s));
+                    }
+
+                    // Set the players list of available abilities to the saved abilities
+                    ps.setAbilities(p_abilities);
                 }
             }
 
+            // Return the fully loaded PlayerState which can be used to fill out the rest of the GameState
+            return ps;
         }
     }
 }
--- PlayerState.cs
+++ PlayerState.cs
@@ -20,6 +20,7 @@ namespace U5Designs
         public string p_equip_ability;
         public string p_name;
         public int p_current_zone;
+        public List<int> p_abilities;
 
         // The default struct constructor which will take the players name and initialize all other values to
         // their default settings.  Use this when creating the initial player state
@@ -31,20 +32,28 @@ namespace U5Designs
             p_equip_ability = "none";
             p_name = playername;
             p_current_zone = 0;
+            p_abilities = new List<int>();
         }
 
         /** Every Player State element will need a Getter here **/
-        public int getHealth(){ return p_health; }
-        public int getDamage(){ return p_damage; }
-        public double getSpeed(){ return p_speed; }
-        public string getAbility(){ return p_equip_ability; }
-        public int getCurrentZone(){ return p_current_zone; }
+        public string getName() { return p_name; }
+        public int getHealth() { return p_health; }
+        public int getDamage() { return p_damage; }
+        public double getSpeed() { return p_speed; }
+        public string getAbility() { return p_equip_ability; }
+        public int getCurrentZone() { return p_current_zone; }
+        public List<int> getAbilities() { return p_abilities; }
 
         /** Every Player State element will need a Setter here **/
+        public void setName(string n) { p_name = n; }
         public void setHealth(int h) { p_health = h; }
         public void setDamage(int d) { p_damage = d; }
         public void setSpeed(double s) { p_speed = s; }
         public void setAbility(string a) { p_equip_ability = a; }
         public void setZone(int z) { p_current_zone = z; }
+        public void setAbilities(List<int> alist) { p_abilities = alist; }
+
+        /** Add a single ability to the player state **/
+        public void addAbility(int a) { p_abilities.Add(a); }
     }
 }
