8c59ad539da2520a05a496e36a0f88e79777f197
Kendal Gifford <solaraeus@gmail.com>
2012-01-24 18:50:15 -0700

    Added GameObject, which is a super class for all in game objects, along with a couple of very minor tweaks in RenderObject

---
+++ Engine/Engine/GameObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+/*
+ * This is the super-class of all in game objects.  It defines
+ * things that are common for everything, like location
+ */
+namespace Engine {
+	class GameObject {
+		//does this object exist when the game is
+		bool existsIn2d() {
+
+			return false;
+		}
+
+		//should this object be rendered when the view is 3d?
+		bool existsIn3d() {
+
+			return false;
+		}
+
+		Vector3 getLocation() {
+
+			return new Vector3(0,0,0);
+		}
+	}
+}
