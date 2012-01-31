0f3da0b12b3bf035aebcc72b617d03a4ee0e565d
Kendal Gifford <solaraeus@gmail.com>
2012-01-31 12:17:31 -0700

    Added class for Obstacle, which is for objects which are rendered and have physics

---
+++ Obstacle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+using OpenTK;
+
+using Engine;
+
+namespace U5Designs {
+	class Obstacle : GameObject, RenderObject, PhysicsObject{
+		public Obstacle(string name, Vector3 location, bool existsIn2d, bool existsIn3d, ObjMesh mesh = null, Bitmap texture = null, SpriteSheet sprite = null) {
+			_obj_name_text = name;
+			_location = location;
+			_existsIn3d = existsIn3d;
+			_existsIn2d = existsIn2d;
+			_mesh = mesh;
+			_texture = texture;
+			_sprite = sprite;
+			_frameNum = 0;
+		}
+
+		private ObjMesh _mesh; //null for sprites
+		ObjMesh RenderObject.mesh {
+			get { return _mesh; }
+		}
+
+		private Bitmap _texture; //null for sprites
+		Bitmap RenderObject.texture {
+			get { return _texture; }
+		}
+
+		private SpriteSheet _sprite; //null for 3d objects
+		SpriteSheet RenderObject.sprite {
+			get { return _sprite; }
+		}
+
+		private int _frameNum; //index of the current animation frame
+		int RenderObject.frameNumber {
+			get { return _frameNum; }
+			set { _frameNum = value; }
+		}
+
+		bool RenderObject.is3d() {
+			throw new Exception("The method or operation is not implemented.");
+		}
+
+		bool RenderObject.isAnimated() {
+			throw new Exception("The method or operation is not implemented.");
+		}
+
+		void RenderObject.doScaleTranslateAndTexture() {
+			throw new Exception("The method or operation is not implemented.");
+		}
+
+		void PhysicsObject.physUpdate(FrameEventArgs e, List<PhysicsObject> objlist) {
+			throw new Exception("The method or operation is not implemented.");
+		}
+
+		void PhysicsObject.accelerate(double acceleration) {
+			throw new Exception("The method or operation is not implemented.");
+		}
+	}
+}
