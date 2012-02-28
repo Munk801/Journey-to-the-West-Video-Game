b707c8946850bd4af0ae440dba83e8db26036fac
SethSLC <walsh1812@gmail.com>
2012-02-27 20:05:54 -0700

    Starting work on GameOver state.  Also changed test_enemy to Boy enemy file, all we need now is the ice cream projectile sprite, and Boy sprite

--- GameOverState.cs
+++ GameOverState.cs
@@ -19,11 +19,22 @@ namespace U5Designs {
         protected Vector3 eye, lookat;
         Obstacle background;
         MainMenuState menu;
+        float xf, yf;
+        Texture go_texture;
 
         public GameOverState(MainMenuState prvstate, GameEngine engine)
         {
             eng = engine;
             menu = prvstate;
+
+            lookat = new Vector3(0, 0, 2);
+            eye = new Vector3(0, 0, 5);
+            xf = 1.0f;
+            yf = 1.0f;
+
+            eng.StateTextureManager.RenderSetup();
+            eng.StateTextureManager.LoadTexture("game_over", "../../Resources/Textures/game_over_text.png");
+            go_texture = eng.StateTextureManager.GetTexture("game_over");
         }
 
 		public override void MakeActive() {
@@ -38,7 +49,21 @@ namespace U5Designs {
         public override void Draw(FrameEventArgs e)
         {
             GL.Clear(ClearBufferMask.AccumBufferBit | ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
+            Matrix4 modelview = Matrix4.LookAt(eye, lookat, Vector3.UnitY);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref modelview);
+            if (xf >= 0.4)
+            {
+                xf -= 0.0001f;
+                yf -= 0.0001f;
+                go_texture.Draw2DTexture(0, 0, xf, yf);
+            }
+            else
+            {
+                go_texture.Draw2DTexture(0, 0, xf, yf);
+            }
 
+            //go_texture.Draw2DTexture(0, 0);
         }
 
         private void DealWithInput()
