7895486b16e09d2aef468c0330b63694ea2a4672
Kendal Gifford <solaraeus@gmail.com>
2012-03-15 14:27:58 -0600

    Forgot a file again.

---
+++ Engine/Engine/SphereRegion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace Engine {
+	public class SphereRegion {
+		private Vector3 center;
+		private float radius;
+		public SphereRegion(Vector3 center, float radius) {
+			this.center = center;
+			this.radius = radius;
+		}
+
+		public bool contains(Vector3 p) {
+			return (p - center).LengthFast <= radius;
+		}
+	}
+}
